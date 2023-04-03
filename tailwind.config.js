b7ff70a93407fcbed2e4d10ceeee24e31c382122
Poșircă Alex <axell.tech7@gmail.com>
2023-04-03 23:54:09 +0300

    Header - Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
+    screens: {
+      md: "768px",
+      lg: "1200px"
+    },
+    colors: {
+      "white-main": "#FAFAF9",
+      "black-main": "#171717",
+      "gray-dark": {
+        100: "#EFEDE8a6",
+        200: "#303030",
+      },
+      "gray-light": {
+        100: "#323443",
+        200: "#232532",
+      },
+      "orange": {
+        100: "#EF8964",
+        200: "#EF8963",
+        300: "#E6533C",
+      },
+      "purple": "#7A29DC",
+      "blue": "#0070C9",
+    },
+    fontFamily: {
+      inter: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
