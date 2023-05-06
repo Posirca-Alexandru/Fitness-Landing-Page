e72ce6d399fba8284585713fdf83f6cf3b5f499c
Poșircă Alex <axell.tech7@gmail.com>
2023-05-06 04:45:01 +0300

    Multistep form navigate with dynamics steps 2.0

--- dist/js/multistep-form.js
+++ dist/js/multistep-form.js
@@ -22,21 +22,21 @@ steps.forEach((step) => {
     prevBtn.addEventListener("click", () => {
       document.querySelector(`.step-${currentStep}`).style.display = "none";
       currentStep--;
-      console.log(currentStep + ' step steps');
+      console.log(currentStep + " step steps");
       document.querySelector(`.step-${currentStep}`).style.display = "flex";
       circleSteps[currentCircle].classList.remove("active");
       currentCircle--;
-      console.log(currentCircle + ' circle steps');
+      console.log(currentCircle + " circle steps");
     });
-}
-nextBtn.addEventListener("click", () => {
+  }
+  nextBtn.addEventListener("click", () => {
     document.querySelector(`.step-${currentStep}`).style.display = "none";
     if (currentStep < 5 && validateForm()) {
-        currentStep++;
-        console.log(currentStep + ' step');
-        currentCircle++;
-        console.log(currentCircle + ' circle');
-    //   setTotal();
+      currentStep++;
+      console.log(currentStep + " step");
+      currentCircle++;
+      console.log(currentCircle + " circle");
+        setTotal();
     }
     document.querySelector(`.step-${currentStep}`).style.display = "flex";
     circleSteps[currentCircle].classList.add("active");
@@ -44,26 +44,18 @@ nextBtn.addEventListener("click", () => {
   });
 });
 
-// const summary = (obj) => {
-//   const planName = document.querySelector(".plan-name");
-//   const planPrice = document.querySelector(".plan-price");
-//   planPrice.innerHTML = `${obj.price.innerText}`;
-//   planName.innerHTML = `${obj.plan.innerText} (${
-//     obj.kind ? "yearly" : "monthly"
-//   })`;
-// };
 
 const validateForm = () => {
   let valid = true;
   for (let i = 0; i < formInputs.length; i++) {
     if (!formInputs[i].value) {
-        valid = false;
-        formInputs[i].classList.add("error");
-        findLabel(formInputs[i]).nextElementSibling.style.display = "flex";
+      valid = false;
+      formInputs[i].classList.add("error");
+      findLabel(formInputs[i]).nextElementSibling.style.display = "flex";
     } else {
-        valid = true;
-        formInputs[i].classList.remove("error");
-        findLabel(formInputs[i]).nextElementSibling.style.display = "none";
+      valid = true;
+      formInputs[i].classList.remove("error");
+      findLabel(formInputs[i]).nextElementSibling.style.display = "none";
     }
   }
   return valid;
@@ -78,12 +70,111 @@ const findLabel = (el) => {
 };
 
 plans.forEach((plan) => {
-    plan.addEventListener('click', () => {
-        document.querySelector('.selected').classList.remove('selected');
-        plan.classList.add('selected');
-        const planName = plan.querySelector('b');
-        const planPrice = plan.querySelector('.plan-priced');
-        obj.plan = planName;
-        obj.price = planPrice;
+  plan.addEventListener("click", () => {
+    document.querySelector(".selected").classList.remove("selected");
+    plan.classList.add("selected");
+    const planName = plan.querySelector("b");
+    const planPrice = plan.querySelector(".plan-priced");
+    obj.plan = planName;
+    obj.price = planPrice;
+});
+});
+
+switcher.addEventListener("click", () => {
+    const val = switcher.querySelector("input").checked;
+    if (val) {
+    document.querySelector(".monthly").classList.remove("sw-active");
+    document.querySelector(".yearly").classList.add("sw-active");
+} else {
+    document.querySelector(".monthly").classList.add("sw-active");
+    document.querySelector(".yearly").classList.remove("sw-active");
+  }
+  switchPrice(val);
+  obj.kind = val;
+});
+
+addons.forEach((addon) => {
+  addon.addEventListener("click", (e) => {
+    const addonSelect = addon.querySelector("input");
+    const ID = addon.getAttribute("data-id");
+    if (addonSelect.checked) {
+      addonSelect.checked = false;
+      addon.classList.remove("ad-selected");
+      showAddon(ID, false);
+    } else {
+      addonSelect.checked = true;
+      addon.classList.add("ad-selected");
+      showAddon(addon, true);
+      e.preventDefault();
+    }
+  });
+});
+
+const switchPrice = (checked) => {
+    const yearlyPrice = [90, 120, 150];
+  const monthlyPrice = [9, 12, 15];
+  const prices = document.querySelectorAll(".plan-priced");
+  if (checked) {
+      prices[0].innerHTML = `$${yearlyPrice[0]}/yr`;
+      prices[1].innerHTML = `$${yearlyPrice[1]}/yr`;
+    prices[2].innerHTML = `$${yearlyPrice[2]}/yr`;
+    setTime(true);
+  } else {
+    prices[0].innerHTML = `$${monthlyPrice[0]}/mo`;
+    prices[1].innerHTML = `$${monthlyPrice[1]}/mo`;
+    prices[2].innerHTML = `$${monthlyPrice[2]}/mo`;
+    setTime(false);
+  }
+};
+
+const showAddon = (ad, val) => {
+  const temp = document.getElementsByTagName("template")[0];
+  const clone = temp.content.cloneNode(true);
+  const serviceName = clone.querySelector(".service-name");
+  const servicePrice = clone.querySelector(".service-price");
+  const serviceID = clone.querySelector(".selected-addon");
+  if (ad && val) {
+    serviceName.innerText = ad.querySelector("label").innerText;
+    servicePrice.innerText = ad.querySelector(".price").innerText;
+    serviceID.setAttribute("data-id", ad.dataset.id);
+    document.querySelector(".addons").appendChild(clone);
+  } else {
+    const addons = document.querySelectorAll(".selected-addon");
+    addons.forEach((addon) => {
+      const attr = addon.getAttribute("data-id");
+      if (attr == ad) {
+          addon.remove();
+      }
     });
-})
+  }
+};
+
+// const summary = (obj) => {
+//   const planName = document.querySelector(".plan-name");
+//   const planPrice = document.querySelector(".plan-price");
+//   planPrice.innerHTML = `${obj.price.innerText}`;
+//   planName.innerHTML = `${obj.plan.innerText} (${
+//     obj.kind ? "yearly" : "monthly"
+//   })`;
+// };
+
+const setTotal = () => {
+    const str = planPrice.innerHTML;
+    const res = str.replace(/\D/g, "");
+  const addonPrices = document.querySelectorAll(
+    ".selected-addon .service-price"
+  );
+  let val = 0;
+  for (let i = 0; i < addonPrices.length; i++) {
+    const str = addonPrices[i].innerHTML;
+    const res = str.replace(/\D/g, "");
+    val += Number(res);
+  }
+  total.innerHTML = `$${val + Number(res)}/${time ? "yr" : "mo"}`;
+};
+
+const setTime = (t) => {
+  return (time = t);
+};
+
+
