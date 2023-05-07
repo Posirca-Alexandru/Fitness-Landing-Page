2a2d9d7b452ab3bee13e0e883da4df223b169240
Poșircă Alex <axell.tech7@gmail.com>
2023-05-07 23:56:58 +0300

    Logout popup

--- dist/js/logout.js
+++ dist/js/logout.js
@@ -3,6 +3,12 @@ import { auth } from "./main.js";
 
 const logoutBtn = document.getElementById("logout");
 const popup = document.getElementById("popup");
+const loggedIn = document.getElementById("logged-in");
+const loggedOut = document.getElementById("logged-out");
+const closePopup = document.getElementById("close-popup");
+
+console.log(loggedOut);
+console.log(loggedIn);
 
 const logoutWithGoogle = () => {
   auth
@@ -22,9 +28,19 @@ logoutBtn.addEventListener("click", logoutWithGoogle);
 onAuthStateChanged(auth, (user) => {
   if (user != null) {
     logoutBtn.style.display = "block";
+    loggedIn.style.display = "block";
   } else {
     logoutBtn.style.display = "none";
-
-    popup.classList.remove("open-popup");
+    popup.classList.add("open-popup");
+    loggedIn.style.display = "none";
+    loggedOut.style.display = "block";
+    closePopup.addEventListener("click", () => {
+      popup.classList.remove("open-popup");
+      // window.location.reload();
+      console.log(location.href)
+    });
+    if(window.location.reload === true) {
+      popup.style.display = "none";
+    }
   }
 });
