d7e5d21248137d0a339093584320703cabeda020
Poșircă Alex <axell.tech7@gmail.com>
2023-05-09 23:13:18 +0300

    Send data from multistep form in database - FireB

--- dist/js/info-list.js
+++ dist/js/info-list.js
@@ -1,27 +1,71 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 import { auth, db } from "./main.js";
-import { getFirestore, doc, setDoc, onSnapshot, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  onSnapshot,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 
-//variables
 const submitJoinBtn = document.getElementById("submit-join");
-const createNameInput = document.getElementById("name-joiner");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const plans = document.querySelectorAll(".plan-card");
+const addons = document.querySelectorAll(".box");
 
-//   let nameTa = createNameInput.value;
-//   if (nameTa && nameTa.length > 0) {
-//     set(ref(db, `membersclub/${user.uid}/infoListMember`), {
-//       name: nameTa,
-//     }); 
-//   }
 
-//auth state
+const member = {
+  plan: null,
+  addons: null,
+  status: "Pending"
+};
+
+
+plans.forEach((plan) => {
+  plan.addEventListener("click", () => {
+    const planName = plan.querySelector("b");
+    const planSelect =
+      planName.innerText + " " + plan.querySelector(".plan-per").innerText;
+    member.plan = planSelect;
+  });
+});
+
+let allAddons = [];
+addons.forEach((addon) => {
+  addon.addEventListener("click", () => {
+    const addonSelect = addon.querySelector("label").innerText;
+    console.log(nameInput.value);
+console.log(member.name);
+    if (allAddons.includes(addonSelect)) {
+      let index = allAddons.indexOf(addonSelect);
+      allAddons.splice(index, 1);
+    } else {
+      allAddons.push(addonSelect);
+    }
+  });
+});
+member.addons = allAddons;
+
 onAuthStateChanged(auth, (user) => {
   const submitJoin = async () => {
-    let refCurrentUser = collection( db, `membersclub/${user.uid}/infoListMember`);
+    let refCurrentUser = collection(
+      db,
+      `membersclub/${user.uid}/memberInfo`
+    );
+
     const docRef = await addDoc(refCurrentUser, {
-      name: createNameInput.value,
+      name: nameInput.value,
+      email: emailInput.value,
+      phone: phoneInput.value,
+      plan: member.plan,
+      addons: member.addons,
+      status: member.status
     })
       .then(() => {
-        alert("merge....");
+        alert("Successfully joining in the fitness club");
       })
       .catch((error) => {
         alert("nu merge..." + error);
