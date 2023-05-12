811205bc8c0d78dca5fc7bb20d5d754ad914e536
Poșircă Alex <axell.tech7@gmail.com>
2023-05-12 23:50:26 +0300

    Send data time and plan value in database

--- dist/js/info-list.js
+++ dist/js/info-list.js
@@ -17,21 +17,44 @@ const plans = document.querySelectorAll(".plan-card");
 const addons = document.querySelectorAll(".box");
 const addonsSelected = document.querySelectorAll("#form-addons");
 const bttnStepThree = document.getElementById("step-3");
+const selectedPlan = document.querySelector(".plan-basic");
+const totalValue = document.querySelector(".total-value");
 
 const member = {
-  plan: null,
   addons: "No addons",
-  status: "Pending",
+  status_join: "Pending",
+  plan: null,
+  date_reg: null,
+  time_reg: null,
+  total_pay_$: null,
 };
 
-plans.forEach((plan) => {
-  plan.addEventListener("click", () => {
-    const planName = plan.querySelector("b");
-    const planSelect =
-      planName.innerText + " " + plan.querySelector(".plan-per").innerText;
-    member.plan = planSelect;
-  });
-});
+// plans.forEach((plan) => {
+//   plan.addEventListener("click", () => {
+//     const planName = plan.querySelector("b");
+//     const planSelect =
+//       planName.innerText + " " + plan.querySelector(".plan-per").innerText;
+//     member.plan = planSelect;
+//   });
+// });
+
+const updateDateTime = () => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  const dateNow = `${day}-${month}-${year}`;
+  const timeNow = `${hours}:${minutes}:${seconds}`;
+
+  member.date_reg = dateNow;
+  member.time_reg = timeNow;
+};
 
 let allAddons = [];
 submitJoinBtn
@@ -42,6 +65,13 @@ submitJoinBtn
           allAddons.push(addonSelect);
         }
       });
+
+      const matchResult = totalValue.innerText.match(/\d+/);
+      const valuePlan = matchResult ? matchResult[0] : "";
+
+      member.total_pay_$ = valuePlan;
+      member.plan = selectedPlan.innerText;
+      updateDateTime();
     })
   : false;
 
@@ -57,7 +87,10 @@ onAuthStateChanged(auth, (user) => {
       phone: phoneInput.value,
       plan: member.plan,
       addons: member.addons,
-      status: member.status,
+      status_join: member.status_join,
+      date_reg: member.date_reg,
+      time_reg: member.time_reg,
+      total_pay_$: member.total_pay_$,
     })
       .then(() => {
         alert("Successfully joining in the fitness club");
